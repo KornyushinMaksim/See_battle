8a03cfc8bbb7da0f920bc62c771f3ab70cacaa57
Maks <ma_korny@yandex.ru>
2023-03-27 00:47:18 +0300

    df

--- see_batle_1/see_batle_1.cpp
+++ see_batle_1/see_batle_1.cpp
@@ -14,42 +14,61 @@ void print_map(char arr[][10], int si_row, int si_cols) {
 }
 //ф-ия расстановка
 void location_ship(char map[][10], int* p_x, int* p_y, int* p_dir, int deck) {
-	int d = rand() % 4;
-	*p_dir = d;
-	int x = *p_x,
-		y = *p_y;
-	for (int j = 0; j < deck; j++) {
-		if (d == 0) {
-			map[x][y] = 79;
-			y--;
+	int cout = 0;
+
+	do {
+		int d = rand() % 4;
+		*p_dir = d;
+		int x = *p_x,
+			y = *p_y;
+		for (int j = 0; j < deck; j++) {
+			if (d == 0) {
+				map[x][y] = 79;
+				y--;
+				if (y < 0 || y >= 9) {
+					break;
+				}
+			}
+			else if (d == 1) {
+				map[x][y] = 79;
+				x++;
+				if (x < 0 || x >= 9) {
+					break;
+				}
+
+			}
+			if (d == 2) {
+				map[x][y] = 79;
+				y++;
+				if (y < 0 || y >= 9) {
+					break;
+				}
+			}
+			if (d == 3) {
+				map[x][y] = 79;
+				x--;
+				if (x < 0 || x >= 9) {
+					break;
+				}
+
+			}
+			cout++;
 		}
-		else if (d == 1) {
-			map[x][y] = 79;
-			x++;
-		}
-		if (d == 2) {
-			map[x][y] = 79;
-			y++;
-		}
-		if (d == 3) {
-			map[x][y] = 79;
-			x--;
-		}
-	}
+	} while (cout < deck);
 }
 //ф-ия проверка
-bool test_location_ship(char map[][10], int* p_x, int* p_y, int* p_dir, int deck) {
-	location_ship(map, p_x, p_y, p_dir, deck);
-
-}
+//bool test_location_ship(char map[][10], int* p_x, int* p_y, int* p_dir, int deck) {
+//	location_ship(map, p_x, p_y, p_dir, deck);
+//
+//}
 
 int main()
 {
-
+	//system("chcp 1251>nul");
 	srand(time(NULL));
 	const int row = 10, cols = 10;
 	char map[row][cols]{};
-	int x, y;
+	int x, y, direction;
 
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < cols; j++) {
@@ -57,8 +76,7 @@ int main()
 		}
 	}
 	//рандомная расстановка кораблей
-	for (int i = 0; i < 10; i++) { //начальная коорд корабля и их кол
-		int direction = 0; //направление
+	for (int i = 0; i < 10; i++) { //начальная коорд корабля и их кол(10 - количество кораблей)
 		x = rand() % row;
 		y = rand() % cols;
 		int* p_x = &x;
@@ -68,17 +86,140 @@ int main()
 			int deck = 4;
 			location_ship(map, p_x, p_y, p_dir, deck);
 		}
-		else if (i == 1 || i == 2) {
-			int deck = 3;
-			location_ship(map, p_x, p_y, p_dir, deck);
-		}
-		else if (i == 3 || i == 4 || i == 5) {
-			int deck = 2;
-			location_ship(map, p_x, p_y, p_dir, deck);
-		}
-		else if (i == 6 || i == 7 || i == 8 || i == 9) {
-			int deck = 1;
-			location_ship(map, p_x, p_y, p_dir, deck);
+		//else if (i == 1 || i == 2) {
+		//	int deck = 3;
+		//	location_ship(map, p_x, p_y, p_dir, deck);
+		//}
+		//else if (i == 3 || i == 4 || i == 5) {
+		//	int deck = 2;
+		//	location_ship(map, p_x, p_y, p_dir, deck);
+		//}
+		//else if (i == 6 || i == 7 || i == 8 || i == 9) {
+		//	int deck = 1;
+		//	location_ship(map, p_x, p_y, p_dir, deck);
+		//}
+	}
+	// обрисовка коробля
+	for (int a = 0; a < row; a++) {
+		for (int b = 0; b < cols; b++) {
+			if (map[a][b] == 79) {
+				if (b == 9) {
+					if (map[a + 1][b] != 79) {
+						map[a + 1][b] = 15;
+					}
+					else {
+						map[a + 1][b] = 79;
+					}
+					if (map[a + 1][b - 1] != 79) {
+						map[a + 1][b - 1] = 15;
+					}
+					else {
+						map[a + 1][b - 1] = 79;
+					}
+					if (map[a][b - 1] != 79) {
+						map[a][b - 1] = 15;
+					}
+					else {
+						map[a][b - 1] = 79;
+					}
+					if (map[a - 1][b - 1] != 79) {
+						map[a - 1][b - 1] = 15;
+					}
+					else {
+						map[a - 1][b - 1] = 79;
+					}
+					if (map[a - 1][b] != 79) {
+						map[a - 1][b] = 15;
+					}
+					else {
+						map[a - 1][b] = 79;
+					}
+				}
+				else if (b == 0) {
+					if (map[a][b + 1] != 79) {
+						map[a][b + 1] = 15;
+					}
+					else {
+						map[a][b + 1] = 79;
+					}
+					if (map[a + 1][b + 1] != 79) {
+						map[a + 1][b + 1] = 15;
+					}
+					else {
+						map[a + 1][b + 1] = 79;
+					}
+					if (map[a + 1][b] != 79) {
+						map[a + 1][b] = 15;
+					}
+					else {
+						map[a + 1][b] = 79;
+					}
+					if (map[a - 1][b] != 79) {
+						map[a - 1][b] = 15;
+					}
+					else {
+						map[a - 1][b] = 79;
+					}
+					if (map[a - 1][b + 1] != 79) {
+						map[a - 1][b + 1] = 15;
+					}
+					else {
+						map[a - 1][b + 1] = 79;
+					}
+
+				}
+				else {
+					if (map[a][b + 1] != 79) {
+						map[a][b + 1] = 15;
+					}
+					else {
+						map[a][b + 1] = 79;
+					}
+					if (map[a + 1][b + 1] != 79) {
+						map[a + 1][b + 1] = 15;
+					}
+					else {
+						map[a + 1][b + 1] = 79;
+					}
+
+					if (map[a + 1][b] != 79) {
+						map[a + 1][b] = 15;
+					}
+					else {
+						map[a + 1][b] = 79;
+					}
+					if (map[a + 1][b - 1] != 79) {
+						map[a + 1][b - 1] = 15;
+					}
+					else {
+						map[a + 1][b - 1] = 79;
+					}
+					if (map[a][b - 1] != 79) {
+						map[a][b - 1] = 15;
+					}
+					else {
+						map[a][b - 1] = 79;
+					}
+					if (map[a - 1][b - 1] != 79) {
+						map[a - 1][b - 1] = 15;
+					}
+					else {
+						map[a - 1][b - 1] = 79;
+					}
+					if (map[a - 1][b] != 79) {
+						map[a - 1][b] = 15;
+					}
+					else {
+						map[a - 1][b] = 79;
+					}
+					if (map[a - 1][b + 1] != 79) {
+						map[a - 1][b + 1] = 15;
+					}
+					else {
+						map[a - 1][b + 1] = 79;
+					}
+				}
+			}
 		}
 	}
 	print_map(map, row, cols);
@@ -135,7 +276,7 @@ int main()
 				}
 			}
 			else {
-				map[a][b] = 254;
+				map[a][b] = 88;
 				cout << "You missed !" << endl;
 			}
 			print_map(map, row, cols);
